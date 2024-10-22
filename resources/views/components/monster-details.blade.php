5b18563640da8b5ab7c280b117811825e2de2171
Josh Santiago-Francia <N00151146@iadt.ie>
2024-10-22 12:53:35 +0100

    edited monster-details and show.blade NOT FINISHED

--- resources/views/components/monster-details.blade.php
+++ resources/views/components/monster-details.blade.php
@@ -1,5 +1,6 @@
 @props(['monster_name', 'alignment', 'challenge_rating', 'armour_class', 'image_url', 'description', 'created_at', 'updated'])
 
 <div class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300 max-w-xl mx-auto"> 
+    <img src="{{ asset('images/monsters/' . $image) }}" alt="{{ _title }}" class="w-200 max w-xs h-auto object-cover">
     <h1 class="font-bold text-black-600 mb-2" style="font-size: 3rem;">{{ $monster_name }}
 </div>
--- resources/views/monsters/show.blade.php
+++ resources/views/monsters/show.blade.php
@@ -1,3 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-x1 text-gray-800 leading-tight">
+            {{ _('All Monsters')}}
+        </h2>
+    </x-slot>
 <div>
     <!-- Order your soul. Reduce your wants. - Augustine -->
 </div>
